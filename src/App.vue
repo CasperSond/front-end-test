16ba39f077c1addf20a47f44dfb209c0bbbd7dd9
Casper Söndergaard <casper.sondergaard@geshdo.com>
2019-09-26 11:45:02 +0200

    removed stuff

--- src/App.vue
+++ src/App.vue
@@ -11,20 +11,6 @@
       </v-btn>
     </v-app-bar>
 
-    <v-carousel>
-      <v-carousel-item v-for="(color, i) in colors" :key="color">
-        <v-sheet :color="color" height="100%" tile>
-          <v-row class="fill-height" align="center" justify="center">
-            <div class="display-3">Slide {{ i + 1 }}</div>
-          </v-row>
-        </v-sheet>
-      </v-carousel-item>
-    </v-carousel>
-
-    <v-row justify="center">
-      <v-date-picker v-model="picker"></v-date-picker>
-    </v-row>
-
     <v-content>
       <HelloWorld />
     </v-content>
